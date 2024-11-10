68cd6c38e1040ca46ec11ec0a8ae0118143c4f34
Adou YAPO AKO <adou.professionnel@gmail.com>
2024-11-10 15:29:15 +0100

    Initiate CRUD operations

--- translator/views.py
+++ translator/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
 #from django_filters import rest_framework as filters
 from translator.models import Translation
 from translator.serializers import TranslationSerializer
 
 # Create your views here.
 
-class TranslationViewSet(viewsets.ModelViewSet):
-    queryset = Translation.objects.all()
-    serializer_class = TranslationSerializer
+class TranslationViewSet(APIView):
+
+    def get(self, request):
+        pass
+    
+    def post(self, request):
+        pass
+    
+    def put(self, request, pk):
+        pass
+    
+    def delete(self, request, pk):
+        pass
