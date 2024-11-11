79c43c49e96a5e76440cce2aaa193d97de54e240
Adou YAPO AKO <adou.professionnel@gmail.com>
2024-11-11 16:31:11 +0100

    Add str formating

--- translator/models.py
+++ translator/models.py
@@ -8,3 +8,6 @@ class Translation(models.Model):
     target_language = models.CharField(max_length=200)
     target_text = models.TextField(default=None)
     creation = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.source_language}-{self.source_text}-{self.target_language}-{self.target_text}"
